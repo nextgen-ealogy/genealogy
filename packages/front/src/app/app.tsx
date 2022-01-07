7b1fec644543ec8b0267a323403e389bf62983c9
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-01-07 20:58:52 +0100

    feat: add leaflet

--- packages/front/src/app/app.tsx
+++ packages/front/src/app/app.tsx
@@ -1,12 +1,14 @@
-import NxWelcome from './nx-welcome';
 import {QueryClient, QueryClientProvider} from "react-query";
 import {useForm} from "react-hook-form";
 import * as yup from "yup";
 import {yupResolver} from "@hookform/resolvers/yup";
 import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import {LatLngExpression} from "leaflet";
+import "./app.module.scss";
 
 i18n
   .use(initReactI18next)
@@ -40,7 +42,11 @@ const schema = yup.object().shape({
 
 export function App() {
   const { t } = useTranslation();
-
+  console.log("APP")
+  const position: LatLngExpression = {
+    lat: 51.505,
+    lng: -0.09
+  }
 
   const {register, handleSubmit, formState: {errors}, reset} = useForm({
     resolver: yupResolver(schema)
@@ -61,6 +67,7 @@ export function App() {
         <GridItem w='100%' h='10' bg='blue.500' />
         <GridItem w='100%' h='10' bg='blue.500' />
       </Grid>
+
       <QueryClientProvider client={queryClient}>
         {t('Welcome to React')}
 
@@ -84,7 +91,12 @@ export function App() {
 
           <button type="submit">Sign in</button>
         </form>
-        <NxWelcome title="front"/>
+        <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
       </QueryClientProvider>
     </ChakraProvider>
 
