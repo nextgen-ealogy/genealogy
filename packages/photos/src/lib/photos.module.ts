c42d242e43c668a2780123b500d320d5cd2814a1
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-04-24 21:10:09 +0200

    fix: reset

--- packages/photos/src/lib/photos.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import {InfrastructureModule} from "./infrastructure/infrastructure.module";
-
-@Module({
-  imports: [InfrastructureModule]
-})
-export class PhotosModule {}
