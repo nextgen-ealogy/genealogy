7b7e23e66ce40518e226c588427af08b51fe3a3e
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-05-06 16:51:20 +0200

    feat: create a neo4j module

--- genealogy/apps/api/src/app/app.module.ts
+++ genealogy/apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { Neo4jModule } from './neo4j/neo4j.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), Neo4jModule],
   controllers: [AppController],
   providers: [AppService],
 })
