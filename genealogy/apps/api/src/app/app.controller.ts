76b260012c5f57dd4225b0bc4d9146d9a017dd15
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2022-05-08 20:32:03 +0200

    fix: create addLink method

--- genealogy/apps/api/src/app/app.controller.ts
+++ genealogy/apps/api/src/app/app.controller.ts
@@ -1,49 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import neo4j from 'neo4j-driver';
-
-export type Neo4jScheme =
-  | 'neo4j'
-  | 'neo4j+s'
-  | 'neo4j+scc'
-  | 'bolt'
-  | 'bolt+s'
-  | 'bolt+scc';
-
-export interface Neo4jConfig {
-  scheme: Neo4jScheme;
-  host: string;
-  port: number | string;
-  username: string;
-  password: string;
-  database?: string;
-}
 
 @Controller()
 export class AppController {
-  constructor(private readonly configService: ConfigService) {
-    this.testNeo4j();
-  }
-
-  async testNeo4j() {
-    
-    /*
-    const person1Name = 'Alice';
-    const person2Name = 'David';
-
-    const session = driver.session();
-    const writeQuery = `MERGE (p1:Person { name: $person1Name })
-                       MERGE (p2:Person { name: $person2Name })
-                       MERGE (p1)-[:KNOWS]->(p2)
-                       RETURN p1, p2`;
-
-    // Write transactions allow the driver to handle retries and transient errors
-    const writeResult = await session.writeTransaction((tx) =>
-      tx.run(writeQuery, { person1Name, person2Name })
-    );
-    console.log(writeResult)*/
-  }
-
   @Get()
   getData() {
     return 'ok';
--- genealogy/apps/api/src/app/neo4j/service/neo4j.service.ts
+++ genealogy/apps/api/src/app/neo4j/service/neo4j.service.ts
@@ -1,23 +1,34 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import neo4j from 'neo4j-driver';
-import { Driver } from "neo4j-driver-core";
+import { Driver, QueryResult } from 'neo4j-driver-core';
 
 @Injectable()
 export class Neo4jService {
-    private driver: Driver;
-    constructor(private readonly configService: ConfigService){
-        const NEO4J_USER = this.configService.get<string>('NEO4J_USER');
-        const NEO4J_PASSWORD = this.configService.get<string>('NEO4J_PASSWORD');
-        const NEO4J_HOST = this.configService.get<string>('NEO4J_HOST');
-    
-        this.driver = neo4j.driver(
-          NEO4J_HOST,
-          neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
-        );
-    
-        // Verify the connection details or throw an Error
-        this.driver.verifyConnectivity();
-    }
+  private driver: Driver;
+  constructor(private readonly configService: ConfigService) {
+    const NEO4J_USER = this.configService.get<string>('NEO4J_USER');
+    const NEO4J_PASSWORD = this.configService.get<string>('NEO4J_PASSWORD');
+    const NEO4J_HOST = this.configService.get<string>('NEO4J_HOST');
 
+    this.driver = neo4j.driver(
+      NEO4J_HOST,
+      neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD)
+    );
+
+    // Verify the connection details or throw an Error
+    this.driver.verifyConnectivity();
+  }
+
+  public addLink(idFrom: string, relation: string, idTo: string): Promise<QueryResult> {
+    const session = this.driver.session();
+      const writeQuery = `MERGE (p1:Person { id: $idFrom })
+                       MERGE (p2:Person { id: $idTo })
+                       MERGE (p1)-[:$relation]->(p2)
+                       RETURN p1, p2`;
+
+      return session.writeTransaction((tx) =>
+        tx.run(writeQuery, { idFrom, relation, idTo })
+      );
+  }
 }
